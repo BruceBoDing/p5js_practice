2243a679906bb345e4e90cacc377f5ba55616103
Bruce Ding <6127900+BruceBoDing@users.noreply.github.com>
2017-08-24 18:54:09 +0800

    mini changes

--- sketch.js
+++ sketch.js
@@ -1,15 +1,23 @@
+var color = {
+  r : 255,
+  g : 0,
+  b : 190
+};
+
 function setup() {
   createCanvas(640, 480);
-  background(150);
+  background(0);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    stroke(255);
-    fill(0);
+  color.r = map(mouseX, 0, width, 0, 255);
+  color.g = 0;
+  color.b = map(mouseY, 0, height, 0, 255);
+  noStroke();
+  if (mouseIsPressed){
+    fill (color.r, color.g, color.b);
   } else {
-  	stroke(0);
-    fill(255);
+    noFill();
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  ellipse (mouseX, mouseY, 10, 10);
 }

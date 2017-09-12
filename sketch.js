7265ee4f92f840d88b5c450501d2bf19471a7b08
Bruce Ding <6127900+BruceBoDing@users.noreply.github.com>
2017-09-12 15:46:02 +0800

    add rita.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,23 @@
+var field;
+var button;
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(400, 300);
+  field = createInput();
+  button = createButton("Get rhymes!");
+  button.mousePressed(findRhymes);
+  background(50);
+  textSize(32);
+  noStroke();
+  fill(255);
 }
-
 function draw() {
-  background(255,255,255);  
-  drawSprites();
+}
+function findRhymes() {
+  background(50);
+  var lexicon = new RiLexicon();
+  var rhymes = lexicon.rhymes(field.value());
+  if (rhymes.length > 0) {
+    text(rhymes.join(" "),
+      10, 10, width-10, height-10);
+  }
 }

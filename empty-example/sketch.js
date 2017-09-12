5288b6c59204d6e7e28b766a49c1b9b0579a64ca
Bruce Ding <6127900+BruceBoDing@users.noreply.github.com>
2017-09-12 15:03:16 +0800

    Full Version P5

---
+++ empty-example/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+createCanvas(1000, 1000);
+}
+
+function draw() {
+  stroke(0);
+  fill(255);
+  ellipse(200,200,50,50);
+}
--- sketch.js
+++ sketch.js
@@ -1,23 +1,9 @@
-var color = {
-  r : 255,
-  g : 0,
-  b : 190
-};
-
 function setup() {
-  createCanvas(640, 480);
-  background(0);
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
 }
 
 function draw() {
-  color.r = map(mouseX, 0, width, 0, 255);
-  color.g = 0;
-  color.b = map(mouseY, 0, height, 0, 255);
-  noStroke();
-  if (mouseIsPressed){
-    fill (color.r, color.g, color.b);
-  } else {
-    noFill();
-  }
-  ellipse (mouseX, mouseY, 10, 10);
+  background(255,255,255);  
+  drawSprites();
 }
